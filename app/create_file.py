8f19d4617bdd6a2bd61bef68430f2abd88518c2e
kekW <panicua12@gmail.com>
2024-04-07 20:44:56 +0300

    minor changes in writing_to_file func

--- app/create_file.py
+++ app/create_file.py
@@ -24,12 +24,12 @@ def terminal_to_file() -> None:
     except argparse.ArgumentError:
         raise argparse.ArgumentError
 
-    d_arguments, f_argument = args.d, args.f
+    d_arguments_list, f_argument_str = args.d, args.f
 
-    if d_arguments:
-        create_folder(d_arguments)
-    if f_argument:
-        create_file(d_arguments, f_argument)
+    if d_arguments_list:
+        create_folder(d_arguments_list)
+    if f_argument_str:
+        create_file(d_arguments_list, f_argument_str)
 
 
 def create_folder(list_args_dir: list) -> None:
@@ -55,17 +55,24 @@ def create_file(list_args_dir: list, file_name: str) -> None:
         writing_to_file(universal_path, "a")
 
 
-def writing_to_file(path: str, mode: str, full_message: str = "") -> None:
+def writing_to_file(path: str, mode: str) -> None:
+    full_message = list()
+
+    while True:
+        message = input("content line: ")
+        if message == "stop":
+            full_message.append("\n")
+            break
+        full_message.append(message + "\n")
+
+    if len(full_message) < 2:
+        return print("You cannot add an empty message")
+
     with open(path, mode) as file:
-        while True:
-            message = input("content line: ") + "\n"
-            if message == "stop\n":
-                full_message += "\n"
-                break
-            full_message += message
-        file.write(datetime.date.strftime(datetime.datetime.now(),
-                                          "%Y-%m-%d %H:%M:%S") + "\n")
-        file.write(full_message)
+        file.writelines(datetime.date.strftime(datetime.datetime.now(),
+                                               "%Y-%m-%d %H:%M:%S") + "\n")
+        file.writelines(full_message)
 
 
-terminal_to_file()
+if __name__ == "__main__":
+    terminal_to_file()
